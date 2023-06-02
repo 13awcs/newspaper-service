cbe20200e1f1f9264ab2059a149066336a9173d5
Bactv <bactv@rabiloo.com>
2023-06-02 17:03:13 +0700

    xcvxcvx

--- src/main/java/com/example/newspaperservice/controller/NewspaperController.java
+++ src/main/java/com/example/newspaperservice/controller/NewspaperController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("newspaper")
 @RequiredArgsConstructor
 public class NewspaperController {
     private final NewspaperService newspaperService;
